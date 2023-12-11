1f8218652cc17e2711dc4fd1abf74bbef9db576c
Nikhil Nygaard <nik@nygaard.site>
2023-12-11 15:56:43 -0700

    54968 is the right answer

---
+++ 1/go.mod
@@ -0,0 +1,3 @@
+module github.com/njnygaard/advent/1
+
+go 1.21.5
--- 1/main.go
+++ 1/main.go
@@ -1,7 +1,70 @@
 package main
 
+import (
+  "bufio"
+  "fmt"
+  "strconv"
+  "log"
+  "os"
+  "unicode"
+)
 
 func main() {
-  fmt.Println("something")
-}
+  file, err := os.Open("./input")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer file.Close()
 
+  scanner := bufio.NewScanner(file)
+  sum := 0
+  // optionally, resize scanner's capacity for lines over 64K, see next example
+  for scanner.Scan() {
+    line := scanner.Text()
+    fmt.Println(line)
+
+    // I want to try and do this in one pass...
+    //var m map[string][]int
+    //m = make(map[string][]int)
+
+    first := -1
+    last := -1
+
+    for _,r := range line {
+      if unicode.IsDigit(r) {
+        if first == -1 {
+          i, err := strconv.Atoi(string(r))
+          if err != nil {
+            fmt.Printf("%q is not a digit I recognize", r)
+          }
+          first = i
+          continue
+        }
+        //last = int(r)
+        i, err := strconv.Atoi(string(r))
+        if err != nil {
+          fmt.Printf("%q is not a digit I recognize", r)
+        }
+        last = i
+        //fmt.Printf("%q looks like a number.\n", r)
+        //fmt.Printf("First: %d   Last: %d\n", first, last)
+      }
+    }
+    if last == -1 {
+      last = first
+    }
+    combined := strconv.Itoa(first) + strconv.Itoa(last)
+    c, err := strconv.Atoi(combined)
+    if err != nil {
+      fmt.Printf("Something Happened")
+    }
+    sum += c
+    fmt.Printf("First: %d   Last: %d\n", first, last)
+  }
+
+  fmt.Printf("Sum: %d\n", sum)
+
+  if err := scanner.Err(); err != nil {
+    log.Fatal(err)
+  }
+}

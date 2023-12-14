a49216d04033adbf44bb785f7ffe4f7283d05065
Nikhil Nygaard <nik@nygaard.site>
2023-12-14 07:53:20 -0700

    break up inputs manually

--- 5/a/main.go
+++ 5/a/main.go
@@ -5,19 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
-  "regexp"
-  "math"
 )
-
-type Game struct{
-  id int
-  winners []int
-  yours []int
-  hits int
-  points int
-}
 
 func main() {
 	file, err := os.Open("./input")
@@ -26,75 +14,22 @@ func main() {
 	}
 	defer file.Close()
 
-  sum := 0
-
 	scanner := bufio.NewScanner(file)
 
-  var games []Game
-  games = make([]Game, 0)
-
+  // The input for this one is not really regular.
+  // I don't see a problem yet massaging the input in to separate files.
+  // We'll see if that bites me in 5b.
 	for scanner.Scan() {
-		line := []rune(scanner.Text())
-    games = append(games, parseInput(line))
+		//line := []rune(scanner.Text())
+		line := scanner.Text()
+    fmt.Println(line)
 	}
-
-  for i:= range games {
-    h := check(games[i])
-    games[i].hits = h
-    games[i].points = powInt(2, games[i].hits-1)
-    sum += games[i].points
-    fmt.Printf("ID: %d\nYours: %v\nWinners: %v\nHits: %d\n\n", games[i].id, games[i].yours, games[i].winners, games[i].hits)
-  }
-
-  fmt.Printf("Sum: %d\n", sum)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func powInt(x, y int) int {
-    return int(math.Pow(float64(x), float64(y)))
-}
-
-func check(g Game)(hits int){
-  for i:= range g.yours {
-    for j:= range g.winners {
-      if g.yours[i] == g.winners[j] {
-        hits++
-      }
-    }
-  }
-  return hits
-}
-
-func parseInput(line []rune)(g Game){
-  parsed := string(line)
-  space := regexp.MustCompile(`\s+`)
-  cleaned := space.ReplaceAllString(parsed, " ")
-  colon := strings.Split(cleaned, ":")
-  gameId := strings.Split(colon[0], " ")
-  id, _ := strconv.Atoi(gameId[1])
-
-  numbers := strings.Split(colon[1], "|")
-  winners := strings.Split(strings.Trim(numbers[0], " "), " ")
-  yours := strings.Split(strings.Trim(numbers[1], " "), " ")
-
-  //fmt.Printf("Winners: %v\n", winners)
-  //fmt.Printf("Yours: %v\n", yours)
-  winnerInts := make([]int, 0)
-  yourInts := make([]int, 0)
-  for i := range winners {
-    iT,_ := strconv.Atoi(winners[i])
-    winnerInts = append(winnerInts, iT)
-  }
-  for i := range yours {
-    iT,_ := strconv.Atoi(yours[i])
-    yourInts = append(yourInts, iT)
-  }
-
-  g.id = id
-  g.winners = winnerInts
-  g.yours = yourInts
-  return g
+func parse(l string){
+  
 }
